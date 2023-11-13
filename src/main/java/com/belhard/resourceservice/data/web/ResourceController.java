ff275f40cc31b563364db300cb9f09a270f2120f
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-11-13 11:24:06 +0300

    Created Web layer

---
+++ src/main/java/com/belhard/resourceservice/data/web/ResourceController.java
@@ -0,0 +1,34 @@
+package com.belhard.resourceservice.data.web;
+
+import com.belhard.resourceservice.service.ResourceService;
+import com.belhard.resourceservice.service.dto.ResourceIdDto;
+import com.belhard.resourceservice.service.dto.ResourceIdsDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@RestController
+@RequestMapping("/resources")
+@RequiredArgsConstructor
+public class ResourceController {
+
+    private final ResourceService resourceService;
+
+    @PostMapping(consumes = "audio/mpeg")
+    public ResourceIdDto upload(InputStream data) throws IOException {
+        return resourceService.upload(data.readAllBytes());
+    }
+
+    @GetMapping("/{id}")
+    public byte[] download(@PathVariable long id) {
+        return resourceService.download(id);
+    }
+
+    @DeleteMapping
+    public ResourceIdsDto delete(@RequestParam("id") List<Long> ids) {
+        return resourceService.delete(ids);
+    }
+}
