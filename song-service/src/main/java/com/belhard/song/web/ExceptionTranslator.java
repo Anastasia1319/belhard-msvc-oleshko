e38b7dde572c747934310a05fd7fdbfe2d29ff93
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-11-13 17:28:17 +0300

    Created web layer in song-service

---
+++ song-service/src/main/java/com/belhard/song/web/ExceptionTranslator.java
@@ -0,0 +1,32 @@
+package com.belhard.song.web;
+
+import com.belhard.song.exceptions.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionTranslator {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void notFound(NotFoundException e) {
+        log.error("Element not found", e);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void notValid(MethodArgumentNotValidException e) {
+        log.error("Element not valid", e);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public void error(Exception e) {
+        log.error("Unknown exception", e);
+    }
+}
---
+++ song-service/src/main/java/com/belhard/song/web/SongController.java
@@ -0,0 +1,35 @@
+package com.belhard.song.web;
+
+import com.belhard.song.service.SongService;
+import com.belhard.song.service.dto.SongDto;
+import com.belhard.song.service.dto.SongIdDto;
+import com.belhard.song.service.dto.SongIdsDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/songs")
+@RequiredArgsConstructor
+public class SongController {
+
+    private final SongService songService;
+
+    @PostMapping
+    public SongIdDto upload(@ModelAttribute SongDto dto) {
+        return songService.save(dto);
+    }
+
+    @GetMapping("/{id}")
+    public SongDto download(@PathVariable long id) {
+        return songService.download(id);
+    }
+
+    @DeleteMapping
+    public SongIdsDto delete(@RequestParam("id") List<Long> ids) {
+        return songService.deleteByResourceIds(ids);
+    }
+
+
+}
