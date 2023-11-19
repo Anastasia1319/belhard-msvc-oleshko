c9b34c010e083127158f29b18ff18ee395953207
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-11-19 17:41:55 +0300

    Created client layer in Resource-service

---
+++ resource-service/src/main/java/com/belhard/resourceservice/client/SongClient.java
@@ -0,0 +1,10 @@
+package com.belhard.resourceservice.client;
+
+import com.belhard.resourceservice.service.dto.MetaDataDto;
+
+import java.util.List;
+
+public interface SongClient {
+    void create (MetaDataDto metaDataDto);
+    void deleteByResourceId(List<Long> resourceIds);
+}
