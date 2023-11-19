4dc2d8dc1f1e417af7073848ce2eca07684fda55
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-11-19 17:48:38 +0300

    Added logging in MetaDataService

--- resource-service/src/main/java/com/belhard/resourceservice/service/impl/MetaDataServiceImpl.java
+++ resource-service/src/main/java/com/belhard/resourceservice/service/impl/MetaDataServiceImpl.java
@@ -2,6 +2,7 @@ package com.belhard.resourceservice.service.impl;
 
 import com.belhard.resourceservice.service.MetaDataService;
 import com.belhard.resourceservice.service.dto.MetaDataDto;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.mp3.Mp3Parser;
@@ -14,15 +15,18 @@ import java.io.IOException;
 import java.time.Duration;
 
 @Service
+@Slf4j
 public class MetaDataServiceImpl implements MetaDataService {
     private static final String DURATION_FORMAT = "%02d:%02d:%02d";
 
     @Override
     public MetaDataDto getMetaData(byte[] data) {
         try {
+            log.info("Method getMetaData was called");
             Metadata metadata = new Metadata();
             Mp3Parser mp3Parser = new Mp3Parser();
             mp3Parser.parse(new ByteArrayInputStream(data), new BodyContentHandler(), metadata);
+            log.info("Data were parsed");
             return buildMetaDataDto(metadata);
         } catch (IOException | SAXException | TikaException e) {
             throw new RuntimeException(e);
@@ -36,6 +40,7 @@ public class MetaDataServiceImpl implements MetaDataService {
         metaDataDto.setAlbum(metadata.get("xmpDM:album"));
         metaDataDto.setLength(getDuration(metadata));
         metaDataDto.setYear(getYear(metadata));
+        log.info("MetaDataDto was created");
         return metaDataDto;
     }
 
