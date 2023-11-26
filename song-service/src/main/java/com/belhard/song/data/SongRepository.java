e597c5f76af3b58f500ae7a4e94838cdcf8570c3
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-11-26 13:17:15 +0300

    Added find-method and delete-method in SongRepository

--- song-service/src/main/java/com/belhard/song/data/SongRepository.java
+++ song-service/src/main/java/com/belhard/song/data/SongRepository.java
@@ -2,6 +2,14 @@ package com.belhard.song.data;
 
 import com.belhard.song.data.entity.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
+
+    Optional<Song> findSongByResourceId(Long resourceId);
+
+    @Transactional
+    void deleteSongByResourceId(Long resourceId);
 }
