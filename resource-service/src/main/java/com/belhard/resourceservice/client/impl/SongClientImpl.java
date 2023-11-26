745e55a2453d1d8bedb9e3ddb98961d4e4a04610
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-11-26 13:04:12 +0300

    Changed uri in SongClientImpl

--- resource-service/src/main/java/com/belhard/resourceservice/client/impl/SongClientImpl.java
+++ resource-service/src/main/java/com/belhard/resourceservice/client/impl/SongClientImpl.java
@@ -2,13 +2,18 @@ package com.belhard.resourceservice.client.impl;
 
 import com.belhard.resourceservice.client.SongClient;
 import com.belhard.resourceservice.service.dto.MetaDataDto;
+import com.netflix.discovery.EurekaClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,10 +23,16 @@ import java.util.stream.Collectors;
 public class SongClientImpl implements SongClient {
 
     private final WebClient webClient;
+    private final EurekaClient eurekaClient;
+    private final LoadBalancerClient loadBalancerClient;
+
+    @Value("${clients.song-service}")
+    private String songServiceId;
 
     @Override
     public void create(MetaDataDto metaDataDto) {
         String response = webClient.post()
+                .uri(getUri())
                 .bodyValue(metaDataDto)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
@@ -33,7 +44,7 @@ public class SongClientImpl implements SongClient {
     @Override
     public void deleteByResourceId(List<Long> resourceIds) {
         String response = webClient.delete()
-                .uri("?id=" + resourceIds.stream()
+                .uri(getUri() + "?id=" + resourceIds.stream()
                         .map(String::valueOf)
                         .collect(Collectors.joining()))
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -41,5 +52,11 @@ public class SongClientImpl implements SongClient {
                 .bodyToMono(String.class)
                 .block();
         log.info("Response to song-service: {}", response);
+    }
+
+    private URI getUri() {
+        ServiceInstance choose = loadBalancerClient.choose(songServiceId);
+        log.info("CHOSEN: getUri {}", choose.getUri());
+        return choose.getUri();
     }
 }
